387fbd5177bd5c6529ebcecdde1b70f40f868a91
Pavel Alekseev <aleks2071@ya.ru>
2019-10-26 09:30:18 +0300

    1 commit
    test

---
+++ app/src/main/java/devlight/io/sample/MainActivity.java
@@ -0,0 +1,102 @@
+package devlight.io.sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.view.View;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    private void initUI() {
+        final View btnHorizontalNtb = findViewById(R.id.btn_horizontal_ntb);
+        btnHorizontalNtb.setOnClickListener(this);
+        final View btnHorizontalCoordinatorNtb = findViewById(R.id.btn_horizontal_coordinator_ntb);
+        btnHorizontalCoordinatorNtb.setOnClickListener(this);
+        final View btnTopHorizontalNtb = findViewById(R.id.btn_horizontal_top_ntb);
+        btnTopHorizontalNtb.setOnClickListener(this);
+        final View btnVerticalNtb = findViewById(R.id.btn_vertical_ntb);
+        btnVerticalNtb.setOnClickListener(this);
+        final View btnSamplesNtb = findViewById(R.id.btn_samples_ntb);
+        btnSamplesNtb.setOnClickListener(this);
+
+
+        startActivity(
+                new Intent(MainActivity.this, HorizontalCoordinatorNtbActivity.class)
+        );
+
+    }
+
+    @Override
+    public void onClick(final View v) {
+        ViewCompat.animate(v)
+                .setDuration(200)
+                .scaleX(0.9f)
+                .scaleY(0.9f)
+                .setInterpolator(new CycleInterpolator())
+                .setListener(new ViewPropertyAnimatorListener() {
+                    @Override
+                    public void onAnimationStart(final View view) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(final View view) {
+                        switch (v.getId()) {
+                            case R.id.btn_horizontal_ntb:
+                                startActivity(
+                                        new Intent(MainActivity.this, HorizontalNtbActivity.class)
+                                );
+                                break;
+                            case R.id.btn_horizontal_top_ntb:
+                                startActivity(
+                                        new Intent(MainActivity.this, TopHorizontalNtbActivity.class)
+                                );
+                                break;
+                            case R.id.btn_horizontal_coordinator_ntb:
+                                startActivity(
+                                        new Intent(MainActivity.this, HorizontalCoordinatorNtbActivity.class)
+                                );
+                                break;
+                            case R.id.btn_vertical_ntb:
+                                startActivity(
+                                        new Intent(MainActivity.this, VerticalNtbActivity.class)
+                                );
+                                break;
+                            case R.id.btn_samples_ntb:
+                                startActivity(
+                                        new Intent(MainActivity.this, SamplesNtbActivity.class)
+                                );
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationCancel(final View view) {
+
+                    }
+                })
+                .withLayer()
+                .start();
+    }
+
+    private class CycleInterpolator implements android.view.animation.Interpolator {
+
+        private final float mCycles = 0.5f;
+
+        @Override
+        public float getInterpolation(final float input) {
+            return (float) Math.sin(2.0f * mCycles * Math.PI * input);
+        }
+    }
+}
---
+++ app/src/main/java/devlight/io/sample/VerticalNtbActivity.java
@@ -0,0 +1,132 @@
+package devlight.io.sample;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import devlight.io.library.ntb.NavigationTabBar;
+
+import java.util.ArrayList;
+
+/**
+ * Created by GIGAMOLE on 28.03.2016.
+ */
+public class VerticalNtbActivity extends Activity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vertical_ntb);
+        initUI();
+    }
+
+    private void initUI() {
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_vertical_ntb);
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 8;
+            }
+
+            @Override
+            public boolean isViewFromObject(final View view, final Object object) {
+                return view.equals(object);
+            }
+
+            @Override
+            public void destroyItem(final View container, final int position, final Object object) {
+                ((ViewPager) container).removeView((View) object);
+            }
+
+            @Override
+            public Object instantiateItem(final ViewGroup container, final int position) {
+                final View view = LayoutInflater.from(
+                        getBaseContext()).inflate(R.layout.item_vp, null, false);
+
+                final TextView txtPage = (TextView) view.findViewById(R.id.txt_vp_item_page);
+                txtPage.setText(String.format("Page #%d", position));
+
+                container.addView(view);
+                return view;
+            }
+        });
+
+        final String[] colors = getResources().getStringArray(R.array.vertical_ntb);
+
+        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_vertical);
+        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_first),
+                        Color.parseColor(colors[0]))
+                        .title("ic_first")
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_second),
+                        Color.parseColor(colors[1]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_second")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_third),
+                        Color.parseColor(colors[2]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_third")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_fourth),
+                        Color.parseColor(colors[3]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_fourth")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_fifth),
+                        Color.parseColor(colors[4]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_fifth")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_sixth),
+                        Color.parseColor(colors[5]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_sixth")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_seventh),
+                        Color.parseColor(colors[6]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_seventh")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_eighth),
+                        Color.parseColor(colors[7]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_eighth")
+                        .build()
+        );
+
+        navigationTabBar.setModels(models);
+        navigationTabBar.setViewPager(viewPager, 4);
+    }
+}
